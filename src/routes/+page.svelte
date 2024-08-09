a4cc7f448125585315678a0d5313883c8babf74a
stonelin <jason840708@gmail.com>
2024-08-09 18:47:41 +0800

    Update driver links and description

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,40 +1,65 @@
 <div class="container mx-auto p-4">
-    <h2 class="text-2xl font-bold mb-6">
-      <div class="">KonteX Software</div>
-    </h2>
+  <div>
+    <h2 class="text-2xl font-bold mb-3">Open Source Software</h2>
+    <h3 class="text-xl mb-3 ml-6">
+      <a
+        class="link link-primary link-hover"
+        href="https://github.com/kontex-neuro/Intan-RHX"
+        title="XDAQ-RHX on GitHub">XDAQ-RHX</a
+      >
+      <span> - Forked from Intan RHX, now exclusively supporting XDAQ</span>
+    </h3>
+    <h3 class="text-xl mb-3 ml-6">
+      <a
+        class="link link-primary link-hover"
+        href="https://github.com/kontex-neuro/XDAQ-OE"
+        title="XDAQ-OE on GitHub">XDAQ-OE</a
+      >
+      <span> - Open Ephys plugin for XDAQ</span>
+    </h3>
+  </div>
 
-    <div>
-      <p class="text-2xl font-bold mb-6">
-        Open Source Softwares
-      </p>
-      <h3 class="text-xl font-bold mb-6 ml-6">
-        <a class="link link-hover" href="https://github.com/kontex-neuro/Intan-RHX">XDAQ-RHX</a>
-        
-      </h3>
-      <h3 class="text-xl font-bold mb-6 ml-6">
-        <a class="link link-hover" href="https://github.com/kontex-neuro/XDAQ-OE">XDAQ-OE</a>
-        
-      </h3>
-    </div>
+  <h2 class="text-2xl font-bold mb-3 mt-3">XDAQ APIs</h2>
 
-    <p class="text-2xl font-bold mb-6">
-      XDAQ APIs
-    </p>
+  <div>
+    <h3 class="text-xl mb-3 ml-6">
+      <a
+        class="link link-primary link-hover"
+        href="https://developer.kontex.io/libxdaq/"
+        title="libxdaq API documentation">libxdaq API Documentation</a
+      >
+      <span> - Interface for controlling different generations of XDAQ</span>
+    </h3>
+    <h3 class="text-xl mb-3 ml-6">
+      <a
+        class="link link-primary link-hover"
+        href="https://github.com/kontex-neuro/libxdaq-dist"
+        title="libxdaq binary distribution">libxdaq-dist</a
+      >
+      <span> - Binary distribution of libxdaq</span>
+    </h3>
+  </div>
 
-    <div>
-      <h3 class="text-xl font-bold mb-6 ml-6">
-        <a class="link link-hover" href="https://developer.kontex.io/libxdaq/">libxdaq</a>
-        
-      </h3>
-      <h3 class="text-xl font-bold mb-6 ml-6">
-        <a class="link link-hover" href="https://github.com/kontex-neuro/libxdaq-dist">libxdaq-dist</a>
-        
-      </h3>
-    </div>
+  <h2 class="text-2xl font-bold mb-3 mt-3">XDAQ Drivers</h2>
+
+  <div>
+    <h3 class="text-xl mb-3 ml-6">
+      <a
+        class="link link-primary link-hover"
+        href="https://drive.google.com/file/d/1aP8nUAXBEV7Ncexygt_Rs6mdWZY8Yau-/view?usp=drive_link"
+        title="XDAQ Gen2 Windows Driver">XDAQ Gen2 Driver (Windows)</a
+      >
+      <span>
+        - Windows driver for XDAQ Gen2, which uses Thunderbolt instead of USB</span
+      >
+    </h3>
+    <h3 class="text-xl mb-3 ml-6">
+      <a
+        class="link link-primary link-hover"
+        href="https://pins.opalkelly.com/downloads"
+        title="OpalKelly FrontPanel USB Drivers">OpalKelly FrontPanel</a
+      >
+      <span> - FrontPanel USB drivers for XDAQ</span>
+    </h3>
+  </div>
 </div>
-
-<style lang="postcss">
-  :global(html) {
-    background-color: theme(colors.gray.100);
-  }
-</style>
