4064881e8b6931e84a65da0001726a7cb7c731cc
Steven Shen <zhexiushen@gmail.com>
2024-08-09 17:25:59 +0800

    Add panel, link to some webpages

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -10,4 +10,70 @@
   });
 </script>
 
+<div class="navbar bg-base-100">
+  <div class="navbar-start">
+    <a href="/">
+      <img class="h-14" src="/KonteX-logo.png" alt="KonteX" />
+    </a>
+    
+    <!-- <div class="dropdown">
+      <div tabindex="0" role="button" class="btn btn-ghost lg:hidden">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          class="h-5 w-5"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor">
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="2"
+            d="M4 6h16M4 12h8m-8 6h16" />
+        </svg>
+      </div>
+      <ul
+        tabindex="0"
+        class="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow">
+        <li><a>Item 1</a></li>
+        <li>
+          <a>Parent</a>
+          <ul class="p-2">
+            <li><a>Submenu 1</a></li>
+            <li><a>Submenu 2</a></li>
+          </ul>
+        </li>
+        <li><a>Item 3</a></li>
+      </ul>
+    </div> -->
+  </div>
+  <!-- <div class="navbar-center hidden lg:flex">
+    <ul class="menu menu-horizontal px-1">
+      <li><a>Item 1</a></li>
+      <li>
+        <details>
+          <summary>Parent</summary>
+          <ul class="p-2">
+            <li><a>Submenu 1</a></li>
+            <li><a>Submenu 2</a></li>
+          </ul>
+        </details>
+      </li>
+      <li><a>Item 3</a></li>
+    </ul>
+  </div> -->
+  <div class="navbar-end">
+    <button
+      class="btn btn-primary"
+      data-toggle-theme="dark,light"
+      data-act-class="ACTIVECLASS">Toggle Theme
+    </button>
+  </div>
+</div>
+
 <slot />
+
+<footer class="fixed inset-x-0 bottom-0 footer bg-neutral text-neutral-content p-10">
+  <aside class="flex">
+    Copyright © {new Date().getFullYear()}, <a class="link link-hover" href="https://kontex.io/">KonteX Neuroscience</a>. All Rights Reserved.
+  </aside>
+</footer>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,12 +1,37 @@
-<button
-  class="btn btn-primary"
-  data-toggle-theme="dark,light"
-  data-act-class="ACTIVECLASS">Toggle Theme</button
->
+<div class="container mx-auto p-4">
+    <h2 class="text-2xl font-bold mb-6">
+      <div class="">KonteX Software</div>
+    </h2>
 
-<h1 class="text-3xl font-bold underline">
-  Hello world!
-</h1>
+    <div>
+      <p class="text-2xl font-bold mb-6">
+        Open Source Softwares
+      </p>
+      <h3 class="text-xl font-bold mb-6 ml-6">
+        <a class="link link-hover" href="https://github.com/kontex-neuro/Intan-RHX">XDAQ-RHX</a>
+        
+      </h3>
+      <h3 class="text-xl font-bold mb-6 ml-6">
+        <a class="link link-hover" href="https://github.com/kontex-neuro/XDAQ-OE">XDAQ-OE</a>
+        
+      </h3>
+    </div>
+
+    <p class="text-2xl font-bold mb-6">
+      XDAQ APIs
+    </p>
+
+    <div>
+      <h3 class="text-xl font-bold mb-6 ml-6">
+        <a class="link link-hover" href="https://developer.kontex.io/libxdaq/">libxdaq</a>
+        
+      </h3>
+      <h3 class="text-xl font-bold mb-6 ml-6">
+        <a class="link link-hover" href="https://github.com/kontex-neuro/libxdaq-dist">libxdaq-dist</a>
+        
+      </h3>
+    </div>
+</div>
 
 <style lang="postcss">
   :global(html) {
