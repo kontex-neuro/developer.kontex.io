d7b487cfb4f2eb7f7610bab8816f3b216fd5ae80
stonelin <jason840708@gmail.com>
2024-11-29 11:30:59 +0800

    Add download link for XDAQ-OE-NP

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -7,11 +7,11 @@
         href="https://github.com/kontex-neuro/Intan-RHX"
         title="XDAQ-RHX on GitHub">XDAQ-RHX</a
       >
-      <span> - Forked from Intan RHX, now exclusively supporting XDAQ.</span>
+      <span> - A fork of Intan RHX, now exclusively supporting XDAQ.</span>
 
       <div class="ml-6 max-w-screen-lg text-lg">
         The latest version of XDAQ-RHX is not yet available on the GitHub
-        Releases page. You can download the pre-built installer from the link
+        Releases page. You can download the pre-built installer using the link
         below:
         <a
           class="link link-primary link-hover"
@@ -26,16 +26,34 @@
         href="https://github.com/kontex-neuro/XDAQ-OE"
         title="XDAQ-OE on GitHub">XDAQ-OE</a
       >
-      <span> - An Open Ephys plugin for XDAQ.</span>
+      <span> - An Open Ephys plugin enabling XDAQ use with Intan headstages.</span>
 
       <div class="ml-6 max-w-screen-lg text-lg">
         The latest version of the XDAQ-OE Plugin is not yet available via the
         Open Ephys plugin installer. You can download the pre-built plugin
-        installer from the link below:
+        installer using the link below:
         <a
           class="link link-primary link-hover"
           href="/downloads/XDAQ-OE-0.2.0-win64.exe">[Windows]</a
         >
+      </div>
+    </div>
+
+    <div class="text-xl mb-3 ml-6">
+      <b>XDAQ-OE-NP</b>
+      <span> - An Open Ephys plugin for using XDAQ with Neuro Pixel headstages.</span>
+
+      <div class="ml-6 max-w-screen-lg text-lg">
+        The latest version of the XDAQ-OE-NP Plugin is not yet available via the
+        Open Ephys plugin installer. You can download the pre-built plugin
+        installer using the link below:
+        <a
+          class="link link-primary link-hover"
+          href="/downloads/XDAQ-OE-NP-0.1.1-win64.exe">[Windows]</a
+        >
+        <p class="text-sm mt-2">
+          <i>Note: The source code for XDAQ-OE-NP will be made available in a future release.</i>
+        </p>
       </div>
     </div>
   </div>
@@ -49,7 +67,7 @@
         href="/libxdaq"
         title="libxdaq API documentation">libxdaq API Documentation</a
       >
-      <span> - Interface for controlling different generations of XDAQ.</span>
+      <span> - The interface for controlling different generations of XDAQ.</span>
     </div>
 
     <div class="text-xl mb-3 ml-6">
@@ -58,7 +76,7 @@
         href="https://github.com/kontex-neuro/libxdaq-dist"
         title="libxdaq binary distribution">libxdaq-dist</a
       >
-      <span> - Binary distribution of libxdaq.</span>
+      <span> - The binary distribution of libxdaq.</span>
     </div>
   </div>
 
@@ -72,7 +90,7 @@
         title="XDAQ Gen2 Windows Driver">XDAQ Gen2 Driver (Windows)</a
       >
       <span>
-        - Windows driver for XDAQ Gen2, which uses Thunderbolt instead of USB.</span
+        - The Windows driver for XDAQ Gen2, which uses Thunderbolt instead of USB.</span
       >
     </h3>
     <h3 class="text-xl mb-3 ml-6">
@@ -92,9 +110,9 @@
       <a
         class="link link-primary link-hover"
         href="/downloads/XDAQ-Firmware-Updater.exe"
-        title="XDAQ Gen2 Windows Driver">XDAQ Gen2 Firmware Updater (Windows)</a
+        title="XDAQ Gen2 Firmware Updater">XDAQ Gen2 Firmware Updater (Windows)</a
       >
-      <span> - Firmware Updater for XDAQ Gen 2 Devices</span>
+      <span> - A firmware updater for XDAQ Gen 2 devices.</span>
     </h3>
   </div>
 </div>
